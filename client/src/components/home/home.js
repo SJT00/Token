a6c837f1ab0421fd8075b525bc9b9c5e3d7d5bfb
SJT00 <saadtaj96@gmail.com>
2020-02-15 08:54:47 -0500

    Moved login to component

---
+++ client/src/components/home/home.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Component } from "react";
+import "./home.scss";
+import logo from "../../icons/forbidden.svg";
+import Form from "../form/form";
+
+export class Home extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  callAPI() {
+    fetch("http://localhost:9000/sendText").then(res => res.text());
+  }
+
+  render() {
+    return (
+      <div className="home">
+        <link
+          href="https://fonts.googleapis.com/css?family=Poppins"
+          rel="stylesheet"
+        />
+        <header className="header">
+          <h1 className="title">Alcoholics Nononymous</h1>
+          <img src={logo} className="logo" alt="logo" />
+          <button onClick={this.callAPI}>Press to send text</button>
+        </header>
+        <Form />
+      </div>
+    );
+  }
+}
+
+export default Home;
